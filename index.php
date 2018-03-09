af700a1e4126f608e5e99d3ca0229acf2120aa99
Nix2k <mihail.bokov@gmail.com>
2018-03-09 20:50:22 +0300

    v0.4

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
 <?php
 	require_once './routines.php';
 	
+	function sort2order ($sort)
+	{
+		switch ($sort) {
+			case 'desc':
+				return ' ORDER BY `description`';
+			case 'status':
+				return ' ORDER BY `is_done`';
+			case 'date':
+				return ' ORDER BY `date_added`';
+		}
+		return '';
+	}
+
+	$sort1 = '';
+	$sort2 = '';
+	
+	if (isset($_GET['sort1'])) {
+		$sort1 = clearInput($_GET['sort1']);
+		$_SESSION['sort1'] = $sort1;
+	}
+	elseif (isset($_SESSION['sort1'])) {
+		$sort1 = clearInput($_SESSION['sort1']);
+	}
+
+	if (isset($_GET['sort2'])) {
+		$sort2 = clearInput($_GET['sort2']);
+		$_SESSION['sort2'] = $sort2;
+	}
+	elseif (isset($_SESSION['sort2'])) {
+		$sort2 = clearInput($_SESSION['sort2']);
+	}
+
 	$user = new User();
 
 	if ($user->isLogedin()) {
@@ -9,8 +41,10 @@
 		} catch (PDOException $e) {
 			echo 'Подключение не удалось: ' . $e->getMessage();
 		}
-		$sql = "SELECT * FROM `task` WHERE `user_id`=".$user->getId( );	
-		$data = $pdo->query($sql);
+		$sql1 = "SELECT * FROM `task` WHERE `user_id`=".$user->getId( ).sort2order($sort1);	
+		$data1 = $pdo->query($sql1);
+		$sql2 = "SELECT * FROM `task` WHERE `assigned_user_id`=".$user->getId( ).sort2order($sort2);	
+		$data2 = $pdo->query($sql2);
 	}
 	else {
 		header('Location: login.php');
@@ -27,7 +61,7 @@
 </head>
 <body>
 
-<h1>Мои задачи</h1>
+<h2>Созданные мной задачи</h2>
 <form action="newtask.php" method="GET">
 	<input type="text" name="desc" placeholder="Описание">
 	<input type="submit" name="newtask" value="Добавить задачу">
@@ -35,14 +69,14 @@
 <table>
 	<tr>
 		<th>id</th>
-		<th>Описание</th>
-		<th>Статус</th>
-		<th>Дата добавления</th>
-		<th colspan="2">Действия</th>
+		<th><a href="index.php?sort1=desc">Описание</a></th>
+		<th><a href="index.php?sort1=status">Статус</a></th>
+		<th><a href="index.php?sort1=date">Дата добавления</a></th>
+		<th colspan="3">Действия</th>
 	</tr>
 <?php
-	if ($data) {
-		foreach ($data as $row) {
+	if ($data1) {
+		foreach ($data1 as $row) {
 			$id = $row['id'];
 			if ($row['is_done']==0) {
 				$is_done = '<span style="color: red;">Не выполнено</span>';
@@ -52,7 +86,37 @@
 				$is_done = '<span style="color: green;">Выполнено</span>';
 				$workflow = "<a href='reopen.php?id=$id'>Открыть заново</a>";
 			}
-			echo "<tr><td>$id</td><td>".$row['description']."</td><td>$is_done</td><td>".$row['date_added']."</td><td>$workflow</td><td><a href='delete.php?id=$id'>Удалить</a></td></tr>";
+			echo "<tr><td>$id</td><td>".$row['description']."</td><td>$is_done</td><td>".$row['date_added']."</td><td>$workflow</td><td><a href='edit.php?id=$id'>Редактировать</a></td><td><a href='delete.php?id=$id'>Удалить</a></td></tr>";
+		}
+	}
+	else {
+		echo "Нет результатов";
+	}
+?>
+</table>
+
+<h2>Назначенные мне задачи</h2>
+<table>
+	<tr>
+		<th>id</th>
+		<th><a href="index.php?sort2=desc">Описание</a></th>
+		<th><a href="index.php?sort2=status">Статус</a></th>
+		<th><a href="index.php?sort2=date">Дата добавления</a></th>
+		<th colspan="3">Действия</th>
+	</tr>
+<?php
+	if ($data2) {
+		foreach ($data2 as $row) {
+			$id = $row['id'];
+			if ($row['is_done']==0) {
+				$is_done = '<span style="color: red;">Не выполнено</span>';
+				$workflow = "<a href='done.php?id=$id'>Выполнено</a>";
+			}
+			else {
+				$is_done = '<span style="color: green;">Выполнено</span>';
+				$workflow = "<a href='reopen.php?id=$id'>Открыть заново</a>";
+			}
+			echo "<tr><td>$id</td><td>".$row['description']."</td><td>$is_done</td><td>".$row['date_added']."</td><td>$workflow</td><td><a href='edit.php?id=$id'>Редактировать</a></td><td><a href='delete.php?id=$id'>Удалить</a></td></tr>";
 		}
 	}
 	else {
--- routines.php
+++ routines.php
@@ -4,11 +4,13 @@
 
 	session_start();
 
-	function clearInput($input) {
+	function clearInput($input) 
+	{
 		return htmlspecialchars(strip_tags($input));
 	}
 
-	function checkPass($password) {
+	function checkPass($password) 
+	{
 		if ((strlen($password)>=8) && ((preg_match('/[a-z]/'))*(preg_match('/[A-Z]/'))*(preg_match('/[0-9]/'))==1)){
 			return true;
 		}
